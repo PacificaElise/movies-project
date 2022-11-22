cf2d0c6f42581930ac3ee421e2d8939bf96600be
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2022-11-23 00:24:34 +0300

    add search

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { Movie } from './Movie'
+import React from 'react';
+import { Movie } from './Movie';
 
 function MovieList(props) {
-	const { movies } = props;
+  const { movies = [] } = props;
 
-	return (
-		<div className='movieList'>
-			{movies.map(movie => (
-				<Movie key={movie.imdbID} {...movie}/>
-			))}
-		</div>
-)
+  return (
+    <div className='movieList'>
+      {movies.length ? (
+        movies.map((movie) => (
+          <Movie
+            key={movie.imdbID}
+            {...movie}
+          />
+        ))
+      ) : (
+        <h4>Nothing found</h4>
+      )}
+    </div>
+  );
 }
 
-export { MovieList }
+export { MovieList };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,27 +5,35 @@ import { Search } from '../components/Search';
 
 function Main() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('http://www.omdbapi.com/?apikey=95bb1ee0&s=matrix')
       .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      });
   }, []);
 
   const searchMovies = (str, type = 'all') => {
+    setLoading(true);
     fetch(
       `http://www.omdbapi.com/?apikey=95bb1ee0&s=${str}${
         type !== 'all' ? `&type=${type}` : ''
       }`
     )
       .then((res) => res.json())
-      .then((data) => setMovies(data.Search));
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      });
   };
 
   return (
     <main className='container content'>
       <Search searchMovies={searchMovies} />
-      {movies.length ? <MovieList movies={movies} /> : <Preloader />}
+      {loading ? <Preloader /> : <MovieList movies={movies} />}
     </main>
   );
 }
