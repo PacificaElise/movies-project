3d3db71f9e069e6b4f43d39b9eb9c0f584ebe833
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2022-11-27 13:09:15 +0300

    add deploy

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
+import React from 'react';
 
 function Movie(props) {
-	const { 
-		Title: title, 
-		Year: year, 
-		imdbID: id, 
-		Type: type, 
-		Poster: poster } = props;
+  const {
+    Title: title,
+    Year: year,
+    imdbID: id,
+    Type: type,
+    Poster: poster,
+  } = props;
 
-  return ( 
-		<div id={id} className="card movie blue-grey darken-4 white-text">
-			<div className="card-image waves-effect waves-block waves-light">
-			{
-				poster === 'N/A' ? (
-					<img className="activator" src={`https://via.placeholder.com/300x450?text=${title}`} alt='poster not found' />
-				) : (
-					<img className="activator" src={poster} alt='poster' />	
-				)
-			}
-			</div>
-			<div className="card-content">
-				<span className="card-title activator">{title}</span>
-				<p>{year} <span className='right'>{type}</span> </p>
-			</div>
-	</div>
-  )
+  return (
+    <div
+      id={id}
+      className='card movie blue-grey darken-4 white-text'
+    >
+      <div className='card-image waves-effect waves-block waves-light'>
+        {poster === 'N/A' ? (
+          <img
+            className='activator'
+            src={`https://via.placeholder.com/300x450?text=${title}`}
+            alt='poster not found'
+          />
+        ) : (
+          <img
+            className='activator'
+            src={poster}
+            alt='poster'
+          />
+        )}
+      </div>
+      <div className='card-content'>
+        <span className='card-title activator'>{title}</span>
+        <p>
+          {year} <span className='right'>{type}</span>{' '}
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export { Movie }
+export { Movie };
